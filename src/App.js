bd26edd28ee7faf20be5ee704bd49260fa086b11
darkknight015 <ujavaid015@gmail.com>
2022-09-16 11:38:48 -0700

    Version 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I have learned to successfully deploy the Git
         </p>
         <a
           className="App-link"
